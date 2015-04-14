daed71eb289b8e5382e70eb5ab76a7ca346d7388
Mohammad-Saad <saad.ash29@gmail.com>
2015-04-14 05:25:26 -0700

    Dequeue tableview

--- ReusableTable/ViewController.swift
+++ ReusableTable/ViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
+    let something = [("List of animated television series of the 1940s and 1950s"),
+        ("List of animated television series of the 1960s"),
+        ("List of animated television series of the 1970s"),
+        ("List of animated television series of the 1980s"),
+        ("List of animated television series of the 1990s")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return something.count
+    }
+    
+    func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        
+        let list = something[indexPath.row]
+        
+        cell.textLabel?.text = list
+        
+        return cell
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
